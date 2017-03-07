11254a64ddd942dc57c5e914ebb4046ad9c36f8c
devcronberg <devcronberg@gmail.com>
2017-03-07 09:07:45 +0100

    ny array sort

--- wwwroot/scripts/solution/015array.js
+++ wwwroot/scripts/solution/015array.js
@@ -11,4 +11,14 @@
     a.sort();
     console.log(a);
 
+    a.sort(function (x, y) {
+        if (x > y)
+            return 1;
+        if (x < y)
+            return -1;
+        return 0;
+    });
+    console.log(a);
+
+
 })();
