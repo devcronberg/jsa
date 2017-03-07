c81251a692b6058c23ac77300034b6528b10320d
devcronberg <devcronberg@gmail.com>
2017-03-07 16:39:49 +0100

    Update 090jQueryAjax.js

--- wwwroot/scripts/solution/090jQueryAjax.js
+++ wwwroot/scripts/solution/090jQueryAjax.js
@@ -5,7 +5,7 @@
         var a = $("#myText").val();
         if (a === "")
             a = 10;
-        $.getJSON("http://fdemo2.cronberg.dk/random.ashx", { antal: a }, function (data) {
+        $.getJSON("http://fdemo2.azurewebsites.net/random.ashx", { antal: a }, function (data) {
             var lst = $("#myList").empty();
             for (var i = 0; i < data.length; i++) {
                 lst.append($("<li />").html(data[i].guid));                
